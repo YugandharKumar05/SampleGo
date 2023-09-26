48da9792a273fe09a2f9731b02b19bab79021fb6
yugandhar <yugandharkumar05@gmail.com>
2023-09-26 11:38:00 +0530

    updated code with http requests

--- main.go
+++ main.go
@@ -3,11 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func main() {
+	// When user visits / respond with hello world
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprint(w, "Hello World!")
+	// })
+	// // when user visit /john respond with hello john
+	// http.HandleFunc("/john", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprint(w, "Hello John!")
+	// })
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World!")
+		parts := strings.Split(r.URL.Path, "/")
+		if len(parts) >= 2 && parts[1] != "" {
+			name := parts[1]
+			//if the user is jack ----return response error 404 not found
+			if name == "jack" {
+				// http.NotFound(w, r)
+				w.WriteHeader(404)
+			} else {
+				//say hello to the user based on the incoming request
+				fmt.Fprintf(w, "Hello, %s!", name)
+			}
+		} else {
+			fmt.Fprint(w, "Hello, World!")
+		}
 	})
 	http.ListenAndServe(":8090", nil)
 }
